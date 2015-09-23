9f662bbe7243bff4516de3c10909f7c3ac80f419
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-09-23 11:05:53 +1000

    Revert "Merge branch 'GPT-57---Clicking-on-a-layer-causes-Javascript-error-which-stops-layers-from-working' into master-ga"
    This reverts commit d24ea0a80f2c9a6baffa9ba68dc138c110bbf583, reversing
    changes made to dd94098cd080423fe10718026b9df91a89afa295.

--- src/main/webapp/portal-core/js/portal/widgets/grid/plugin/RowExpanderContainer.js
+++ src/main/webapp/portal-core/js/portal/widgets/grid/plugin/RowExpanderContainer.js
@@ -5,7 +5,7 @@
  *
  * To use this plugin, assign the following field to the plugin constructor
  * {
- *  generateContainer : function(record, parentElId, grid) - returns Ext.container.Container,
+ *  generateContainer : function(record, parentElId) - returns Ext.container.Container,
  *  allowMultipleOpen : Boolean - whether multiple containers can be open simultaneously.
  *  toggleColIndexes : int[] - Optional - Which column indexes can toggle open/close on single click - Defaults to every column 
  * }
@@ -21,7 +21,7 @@
  *                renderTo: 'foo',
  *                plugins : [{
  *                  ptype : 'rowexpandercontainer',
- *                  generateContainer : function(record, parentElId, grid) {
+ *                  generateContainer : function(record, parentElId) {
  *                     return Ext.create('Ext.panel.Panel', {});
  *                  }
  *                }]
@@ -186,23 +186,21 @@ Ext.define('portal.widgets.grid.plugin.RowExpanderContainer', {
         }
     },
     
-    restoreRowContainer: function(record) {        
-        var me = this;
-        
+    restoreRowContainer: function(record) {
         //We don't want this function to be re-entrant
         //Which can occur if the generateContainer callback
         //makes any updates to record
-        if (me.generationRunning === true) {
+        if (this.generationRunning === true) {
             return;
         }
         
-        me.generationRunning = true;
-        if (me.restorationRequired(record)) {
-            var id = Ext.DomQuery.selectNode('div[id^=rowexpandercontainer-' + record.id  + '-]').id;
+        this.generationRunning = true;
+        if (this.restorationRequired(record)) {
+            var id = "rowexpandercontainer-" + record.id;
+            var container = this.generateContainer(record, id);
             
-            me.recordStatus[record.id].container = me.generateContainer(record, id, me.grid);
-            me.recordStatus[record.id].idCounter += 1
-            me.recordStatus[record.id].container.updateLayout({
+            this.recordStatus[record.id].container = container;
+            this.recordStatus[record.id].container.updateLayout({
                 defer:false,
                 isRoot:false
             });
