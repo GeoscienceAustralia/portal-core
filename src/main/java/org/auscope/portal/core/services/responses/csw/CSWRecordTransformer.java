beb48eb1abcdbdc5f9b25912a845d58e74ebbf3e
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-12-06 14:17:44 +1100

    Revert "Gpt 103 fix order anded layers au scope"

--- src/main/java/org/auscope/portal/core/services/responses/csw/CSWRecordTransformer.java
+++ src/main/java/org/auscope/portal/core/services/responses/csw/CSWRecordTransformer.java
@@ -50,7 +50,6 @@ public class CSWRecordTransformer {
     private static final String LANGUAGEEXPRESSION = "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:language/gco:CharacterString";
     private static final String OTHERCONSTRAINTSEXPRESSION = "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:resourceConstraints/gmd:MD_LegalConstraints/gmd:otherConstraints/gco:CharacterString";
     private static final String DATAQUALITYSTATEMENTEXPRESSION = "gmd:dataQualityInfo/gmd:DQ_DataQuality/gmd:lineage/gmd:LI_Lineage/gmd:statement/gco:CharacterString";
-    private static final String LAYERNAME = "gmd:distributionInfo/gmd:MD_Distribution/gmd:transferOptions/gmd:MD_DigitalTransferOptions/gmd:onLine/gmd:CI_OnlineResource/gmd:name/gco:CharacterString";
 
     /**
      * Creates a new instance of this class and generates an empty document that will be used for constructing DOM.
@@ -539,9 +538,8 @@ public class CSWRecordTransformer {
         record.setSupplementalInformation(evalXPathString(this.mdMetadataNode, SUPPLEMENTALINFOEXPRESSION));
         record.setLanguage(evalXPathString(this.mdMetadataNode, LANGUAGEEXPRESSION));
         record.setDataQualityStatement(evalXPathString(this.mdMetadataNode, DATAQUALITYSTATEMENTEXPRESSION));
-        record.setLayerName(evalXPathString(this.mdMetadataNode, LAYERNAME));
 
-        String resourceProvider = evalXPathString(this.mdMetadataNode, RESOURCEPROVIDEREXPRESSION);
+        String resourceProvider = (String) evalXPathString(this.mdMetadataNode, RESOURCEPROVIDEREXPRESSION);
         if (resourceProvider == null || resourceProvider.isEmpty()) {
             resourceProvider = "Unknown";
         }
@@ -559,7 +557,7 @@ public class CSWRecordTransformer {
         }
 
         //There can be multiple gmd:onLine elements (which contain a number of fields we want)
-        tempNodeList = evalXPathNodeList(this.mdMetadataNode, ONLINETRANSFERSEXPRESSION);
+        tempNodeList = (NodeList) evalXPathNodeList(this.mdMetadataNode, ONLINETRANSFERSEXPRESSION);
         List<AbstractCSWOnlineResource> resources = new ArrayList<AbstractCSWOnlineResource>();
         for (int i = 0; i < tempNodeList.getLength(); i++) {
             try {
@@ -574,7 +572,7 @@ public class CSWRecordTransformer {
         record.setOnlineResources(resources.toArray(new AbstractCSWOnlineResource[resources.size()]));
 
         //Parse our bounding boxes (if they exist). If any are unparsable, don't worry and just continue
-        tempNodeList = evalXPathNodeList(this.mdMetadataNode, BBOXEXPRESSION);
+        tempNodeList = (NodeList) evalXPathNodeList(this.mdMetadataNode, BBOXEXPRESSION);
         if (tempNodeList != null && tempNodeList.getLength() > 0) {
             List<CSWGeographicElement> elList = new ArrayList<CSWGeographicElement>();
             for (int i = 0; i < tempNodeList.getLength(); i++) {
@@ -591,7 +589,7 @@ public class CSWRecordTransformer {
         }
 
         //Parse the descriptive keywords
-        tempNodeList = evalXPathNodeList(this.mdMetadataNode, KEYWORDLISTEXPRESSION);
+        tempNodeList = (NodeList) evalXPathNodeList(this.mdMetadataNode, KEYWORDLISTEXPRESSION);
         if (tempNodeList != null && tempNodeList.getLength() > 0) {
             List<String> keywords = new ArrayList<String>();
             Node keyword;
@@ -614,7 +612,7 @@ public class CSWRecordTransformer {
         }
 
         //Parse any legal constraints
-        tempNodeList = evalXPathNodeList(this.mdMetadataNode, OTHERCONSTRAINTSEXPRESSION);
+        tempNodeList = (NodeList) evalXPathNodeList(this.mdMetadataNode, OTHERCONSTRAINTSEXPRESSION);
         if (tempNodeList != null && tempNodeList.getLength() > 0) {
             List<String> constraintsList = new ArrayList<String>();
             Node constraint;
